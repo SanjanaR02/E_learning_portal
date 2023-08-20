cac6b9248b0f73c8586d89ef973489e2685dfe03
SanjanaR02 <108140659+SanjanaR02@users.noreply.github.com>
2023-08-20 12:04:00 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,504 @@
+<?php
+
+
+session_start();
+
+if (!isset($_SESSION['username'])) {
+	header('location:login.php');
+	# code...
+
+}
+
+
+
+?>
+
+
+<!DOCTYPE html>
+<html>
+<head>
+	<link href="https://fonts.googleapis.com/css?family=Aleo" rel="stylesheet">
+
+	<link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro" rel="stylesheet">
+	<title>Akshara</title>
+	<!----magnific popup css file for work section -->
+	<link rel="stylesheet" type="text/css" href="css/magnific-popup.css">
+
+	<!----owlcarousel css file for our team section -->
+	<link rel="stylesheet" type="text/css" href="css/owl.carousel.min.css">
+	<link rel="stylesheet" type="text/css" href="css/owl.theme.default.min.css">
+
+
+	<!----Linking google fonts-->
+	<link href="https://fonts.googleapis.com/css?family=Lobster" rel="stylesheet">
+
+	<!----font-awsome start-->
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+	<!----font-awsome ends-->
+
+		<!----css file link-->
+	<link rel="stylesheet" type="text/css" href="css/stylz.css">
+
+	   <!----favicon setting-->
+	<link rel="shortcut icon" type="text/css" href="img/mylogo.png">
+
+	<!-- Latest compiled and minified CSS -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+
+	<!-- jQuery library -->
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+
+	<!----magnific popup js file for work section -->
+	<script type="text/javascript" src="js/jquery.magnific-popup.min.js"></script>
+
+	<!----owlcarousel js file for our team section -->
+	<script type="text/javascript" src="js/owl.carousel.min.js"></script>
+	<script type="text/javascript" src="js/owl.carousel.js"></script>
+
+	<!-- Latest compiled JavaScript -->
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
+	<!----------email notification-------->
+
+
+
+
+
+
+
+<!-- <link rel="stylesheet" href="assets/tether/tether.min.css">
+ 
+  <link rel="stylesheet" href="assets/theme/css/style.css">
+  <link rel="stylesheet" href="assets/mobirise/css/mbr-additional.css" type="text/css"> -->
+ 
+
+
+
+
+
+	<style type="text/css">
+	
+	.servicebody
+	{
+		
+
+	}
+
+
+	html
+	{
+		scroll-behavior: smooth;
+	}
+	</style>
+
+</head>
+<body onload="myfunction()">
+		   <!---preloader starts	----->
+
+		   <div id="loading"></div>
+
+		   <!---preloader Ends	----->
+
+
+			<!---Navigation Starts	----->
+
+	<nav class="navbar navbar-inverse navbar-fixed-top">
+		<div class="container">
+			<div class="navbar-header">
+				<!------Responsive Button---->
+				<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navi">
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+
+
+				</button>
+
+				<h1 style="color: white;font-weight: 800;text-transform: uppercase;margin-top: 10px;" id="myhead">Akshara</h1>
+			</div>
+			<div class="collapse navbar-collapse" id="navi">
+                 <!------Navigation menus starts---->
+				<ul class="nav navbar-nav navbar-right">
+					<li> <a href="">Home</a></li>
+					<li> <a href="#about">About</a></li>
+					<li> <a href="#work">Courses</a></li>
+					<li> <a href="#contact">Contact US</a></li>
+					<li> <a href="logout.php" id="our-location" class="btn-success" ><?php echo $_SESSION['username'];   ?></a></li>
+				</ul>
+	                 <!------Navigation menus ends---->
+			</div>
+		</div>
+	</nav>
+			<!---Navigation Ends	----->
+
+			<!---Slider Section starts	------->
+			<section class="slider text-center" id="slider">
+				<div class="slider-overlay">
+					<div class="slider-content">
+						<div class="icons">
+
+							<i class="fa fa-apple"></i>
+							<i class="fa fa-android"></i>
+							<i class="fa fa-windows"></i>
+						</div>
+						<br>
+						<div class="text">      <!-- jquery typed effect -->
+							
+						</div>
+							<div class="cta-div">
+								<a href="#myservice_section" class="btn2">LEARN TODAY</a>
+							</div>
+							<br><br>
+							<div class="social-networks">
+								<a href="https://www.facebook.com" class="fa fa-facebook"></a>
+								<a href="https://www.twitter.com" class="fa fa-twitter"></a>
+								<a href="https://www.instagram.com" class="fa fa-instagram"></a>
+								<a href="https://www.linkedin.com" class="fa fa-linkedin"></a>
+							</div>
+					</div>
+				</div>
+			</section>
+
+			<!---Slider Section ends------->
+
+			<!---Login Start------->
+
+			<div class="modal fade modal-dialog-centered" id="mymodal">
+				<div class="modal-dialog ">
+					<div class="modal-content">
+						<h3 id="login-heading">Login</h3>
+						
+					<div class="modal-body" >
+						<div class="left-box">
+						<form method="POST" action="validation.php">
+							<div class="form-group">
+								<label><i class="fa fa-user fa-2x"></i>Username :</label>
+								<input type="text" name="name" class="form-control">
+
+								<label><i class="fa fa-lock fa-2x"></i>Password :</label>
+								<input type="password" name="password" class="form-control">
+								<button id="btn-login" type="submit">Login</button>
+								
+							</div>
+							<div class="register">
+								<h2>Don't have an account?&nbsp<span id="create-account"><a href="signup.html">Create</span></a> </h2>
+							</div>
+							
+						</form>
+					</div>
+					<div class="right-box">
+						<span class="signinwith">Sign in With <br> Social Networks</span>
+
+						<button class="social facebook">Log in with Facebook</button>
+						<button class="social twitter">Log in with twitter</button>
+						<button class="social google">Log in with gmail</button>
+					</div>
+						
+					</div>
+					
+						
+				</div>
+			</div>
+		</div>
+
+         <!---Login Ends------->
+
+         <!---Our Services Section Start------->
+
+         <br><br>
+         <div class="container-fluid servicebody" id="myservice_section">
+         <div class="service-are" id="service">
+         	<div class="row">
+         		<div class="col-xs-12">
+         			<div class="section-title text-center">
+         				<h1><b>Hello learners!!</b></h1>
+         				<h4>
+         					Here you can learn at your own comfort .... gain knowledge and strive into the future !<br>
+         					we believe our courses will help you reach greater heights and thank you for choosing us! ...HAPPY LEARNING!!!
+         				</h4>
+         			</div>
+         		</div>
+         	</div>
+         	<div class="row">
+         		<div class="col-md-4 col-sm-6 col-xs-12">
+         			<div class="service-wrap text-center">
+         				<div class="service-icon">
+         					<i class="fa fa-leaf"></i>
+         				</div>
+         				<h3><a href="programmingdemo.php">PROGRAMMING</a></h3>
+         				<p>
+         					Here you will find all the lecture tutorials related to programming languages 
+         					like JAVA,PYTHON,ANDROID etc
+         				</p>
+         			</div>
+         		</div>
+
+         		<div class="col-md-4 col-sm-6 col-xs-12">
+         			<div class="service-wrap text-center">
+         				<div class="service-icon">
+         					<i class="fa fa-laptop"></i>
+         				</div>
+         				<h3><a href="video tutorials\java\display_video_courses.php">VIDEO TUTORIALS</a></h3>
+         				<p>
+         					Here you will find all the videos tutorials related to programming languages 
+         					like JAVA,PYTHON,ANDROID etc
+         				</p>
+         			</div>
+         		</div>
+
+         		<div class="col-md-4 col-sm-6 col-xs-12">
+         			<div class="service-wrap text-center">
+         				<div class="service-icon">
+         					<i class="fa fa-laptop"></i>
+         				</div>
+         				<h3><a href="online_quize/quizhome.php">EXERCISE</a></h3>   <!--  exercise/exercise.php -->
+         				<p>
+         					Here you will find problem programs for practice and their implementation also which will improve your coding skill
+         				</p>
+         			</div>
+         		</div>
+<!-- 
+         		<div class="col-md-4 col-sm-6 col-xs-12">
+         			<div class="service-wrap text-center">
+         				<div class="service-icon">
+         					<i class="fa fa-laptop"></i>
+         				</div>
+         				<h3><a href="">WEB DESIGN</a></h3>
+         				<p>
+         					this is our serices theses are the services provided by us <br>this are the services provided by us
+         				</p>
+         			</div>
+         		</div>
+ -->
+         		<!-- <div class="col-md-4 col-sm-6 col-xs-12">
+         			<div class="service-wrap text-center">
+         				<div class="service-icon">
+         					<i class="fa fa-laptop"></i>
+         				</div>
+         				<h3><a href="">COMPUTER SCIENCE</a></h3>
+         				<p>
+         					this is our serices theses are the services provided by us <br>this are the services provided by us
+         				</p>
+         			</div>
+         		</div>
+ -->
+         		<!-- <div class="col-md-4 col-sm-6 col-xs-12">
+         			<div class="service-wrap text-center">
+         				<div class="service-icon">
+         					<i class="fa fa-user"></i>
+         				</div>
+         				<h3><a href="">TECH NEWS</a></h3>
+         				<p>
+         					this is our serices theses are the services provided by us <br>this are the services provided by us
+         				</p>
+         			</div>
+         		</div> -->
+         	</div>
+         </div>
+     </div>
+
+			<!---Our Services Section Ends------->
+
+			<!---Emailnotification Section Start------->
+
+
+
+			<!---Emailnotification Section Start------->
+
+
+			<!---Our Services Section Ends------->
+
+			<section class="work" id="work"><br>
+				<h2 id="work-heading" class="text-center" style="font-weight: bold;">COURSES</h2>
+				<h4 class="text-center">We have various courses available here please checkout!</h4>
+				<div class="container-fluid">
+					<!---first row start-->
+					<div class="row no-gutters">
+					<div class="col-md-3 col-sm-3 col-xs-3">
+						<div class="img-wrapper">
+								<img src="img/ht.jpg" class="img-responsive">
+							</a>
+							
+						</div>
+						
+					</div>
+
+					<div class="col-md-3 col-sm-3 col-xs-3">
+						<div class="img-wrapper">
+								<img src="img/ja.jpg" class="img-responsive">
+							</a>
+							
+						</div>
+						
+					</div>
+
+					<div class="col-md-3 col-sm-3 col-xs-3">
+						<div class="img-wrapper">
+								<img src="img/py.jpg" class="img-responsive">
+							</a>
+							
+						</div>
+						
+					</div>
+
+					<div class="col-md-3 col-sm-3 col-xs-3">
+						<div class="img-wrapper">
+								<img src="img/php.jpg" class="img-responsive">
+							</a>
+							
+						</div>
+						
+					</div>
+
+
+					
+					<!---first row ends-->
+
+
+					<!-------second row starts  --->
+					
+					<div class="col-md-3 col-sm-3 col-xs-3">
+						<div class="img-wrapper">
+								<img src="img/css.jpg" class="img-responsive">
+							</a>
+							
+						</div>
+						
+					</div>
+
+					<div class="col-md-3 col-sm-3 col-xs-3">
+						<div class="img-wrapper">
+								<img src="img/sql.jpg" class="img-responsive">
+							</a>
+							
+						</div>
+						
+					</div>
+
+					<div class="col-md-3 col-sm-3 col-xs-3">
+						<div class="img-wrapper">
+								<img src="img/js.jpg" class="img-responsive">
+							</a>
+							
+						</div>
+						
+					</div>
+
+					<div class="col-md-3 col-sm-3 col-xs-3">
+						<div class="img-wrapper">
+								<img src="img/cpp.jpg" class="img-responsive">
+							</a>
+							
+						</div>
+						
+					</div>
+
+					
+					
+					<!---second row ends-->
+					
+				</div>
+				
+
+
+			</section>
+
+
+<!-- =============================================================================================================================== -->
+
+
+
+
+<!-- =============================================================================================================================== -->
+
+
+			<!--Contact us Section Start------->
+
+
+<!------ Include the above in your HEAD tag ---------->
+
+<<div>
+<br>
+      <h2 class="title"><center>Reach out to us</center></h2>
+      <div class="contact" id="contact" style="padding-bottom: 10px !important;">
+      <div class="contact-list contact-address"  >
+        <h5><span><i class="fa fa-map-marker w3-xlarge w3-text-light-grey"></i></span>Address</h5>
+        <p>Saveetha Nagar, Chennai, 602105 </p>
+      </div>
+      <div class="contact-list">
+        <h5><span><i class="fa fa-envelope w3-xlarge w3-text-light-grey"></i></span>Receive weekly update email</h5>
+        <div class="email-contact">
+          <input placeholder="Email"  type="email" style="margin: 8px 0px !important;"/>
+          
+          <button class="w3-btn w3-red w3-round " style="width: 100px; padding:3px 10px !important">Subscribe</button>
+        </div>
+  
+      </div>
+      <div class="contact-list" >
+        <h5><span><i class="fa fa-phone w3-xlarge w3-text-light-grey"></i></span>Phone</h5>
+        <p>  ----------</p>
+      </div>
+
+      </div>
+    </div>
+
+
+    
+
+
+
+			<!---Contact us Section Ends------->
+
+
+
+
+ 			<!---footer Section Start	----->
+
+
+
+ 			<!---footer Section Ends	----->
+
+
+
+  			<!---This is script section------->
+
+<script type="text/javascript">
+	
+	var preloader=document.getElementById('loading');
+	function myfunction()
+	 {
+		preloader.style.display='none';
+	}
+
+
+	function addButton() {
+		var body=document.getElementsByTagName('body')[0];
+		var myfaq1=document.getElementById('myfaq');
+		var btn=document.createElement('button');
+		btn.innerHTML='Sanjana';
+		myfaq1.appendChild(btn);
+		body.appendChild(myfaq);
+	}
+
+
+
+</script>
+
+<script src="js/jquery.ripples-min.js" type="text/javascript"></script>
+<script src="js/typed.min.js" type="text/javascript"></script>
+<script src="js/main.js" type="text/javascript"></script>
+
+
+
+
+
+
+<!--   <script src="assets/popper/popper.min.js"></script>
+  <script src="assets/tether/tether.min.js"></script>
+  <script src="assets/bootstrap/js/bootstrap.min.js"></script>
+  <script src="assets/smoothscroll/smooth-scroll.js"></script>
+  <script src="assets/theme/js/script.js"></script> -->
+
+</body>
+</html>
